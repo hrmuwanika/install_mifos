68ea0859d58f1e2aaaabdaba23cd1a014367b542
Henry Robert Muwanika <hrmuwanika@gmail.com>
2023-10-25 13:39:51 +0200

    Update install_mifos_ubuntu.sh

--- install_mifos_ubuntu.sh
+++ install_mifos_ubuntu.sh
@@ -3,15 +3,26 @@
 # Tomcat installation on Ubuntu: 20.04 
 # ==============================
 
-# Update ubuntu
+# Installing System Updates and Prerequisites
 sudo apt update && sudo upgrade -y
+sudo apt install python-software-properties software-properties-common -y
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
 
-# Install OpenJDK
-sudo apt install default-jdk
+# Install Java
+sudo apt install oracle-java8-installer -y
+sudo update-alternatives --config java
 
-# Create Tomcat User and Group (Do run as root). Create a new group and system user to run the Apache Tomcat service from the /opt/tomcat directory.
+sudo cat <<EOF > .bashrc 
+export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+export CATALINA_HOME=/usr/share/tomcat9
+EOF
+
+source ~/.bashrc
+
+# Create Tomcat User and Group (Do run as root). Create a new group and system user to run the Apache Tomcat service from the /usr/share/tomcat9 directory.
 sudo groupadd tomcat
-sudo useradd -s /bin/false -g tomcat -d /opt/tomcat tomcat
+sudo useradd -s /bin/false -g tomcat -d /usr/share/tomcat9 tomcat
 
 # Set directory to download the tomcat
 cd /tmp
@@ -19,21 +30,21 @@ cd /tmp
 # Now download tomcat.
 curl -O https://downloads.apache.org/tomcat/tomcat-9/v9.0.36/bin/apache-tomcat-9.0.36.tar.gz
 
-# To extract the tar.gz Tomcat file, create a new /opt/tomcat/ directory with the command:
-sudo mkdir /opt/tomcat
+# To extract the tar.gz Tomcat file, create a new /usr/share/tomcat9/ directory with the command:
+sudo mkdir /usr/share/tomcat9
 
 # Now extract tomcat tarbal using the command
-sudo tar xzvf apache-tomcat-9.0.36.tar.gz -C /opt/tomcat --strip-components=1
+sudo tar xzvf apache-tomcat-9.0.36.tar.gz -C /usr/share/tomcat9 --strip-components=1
 
 
 # Modify Tomcat User Permission
 # =============================
 
 # Move to the directory where the Tomcat installation is located:
-cd /opt/tomcat
+cd /usr/share/tomcat
 
 # Grant group ownership over the installation directory to the tomcat group with the command:
-sudo chgrp -R tomcat /opt/tomcat
+sudo chgrp -R tomcat /usr/share/tomcat
 
 # Give it read access to the conf directory and its contents by typing:
 sudo chmod -R g+r conf
@@ -53,8 +64,6 @@ sudo update-java-alternatives -l
 # Create and open a new file in the /etc/system/system under the name tomcat.service:
 sudo cat <<EOF >  /etc/systemd/system/tomcat.service
 
-#  Once the file opens, copy and paste the content below, changing the JAVA_HOME value to the information you found in the previous step
-
 [Unit]
 Description=Apache Tomcat Web Application Container
 After=network.target
@@ -64,8 +73,8 @@ Type=forking
 
 Environment=JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64
 Environment=CATALINA_PID=/opt/tomcat/latest/temp/tomcat.pid
-Environment=CATALINA_HOME=/opt/tomcat
-Environment=CATALINA_BASE=/opt/tomcat
+Environment=CATALINA_HOME=/usr/share/tomcat9
+Environment=CATALINA_BASE=/usr/share/tomcat9
 Environment=’CATALINA_OPTS=-Xms512M –Xmx1024M –server –XX:+UserParallelGC’
 Environment=’JAVA_OPTS=-Djava.awt.headless=true Djava.security.egd=file:/dev/./urandom’
 
@@ -163,3 +172,27 @@ allow=”127\.\d+\.\d+\.\d+|::1|0000:1|THE.IP.ADDRESS.” />
 sudo nano /opt/tomcat/latest/webapps/host-manager/META-INF/context.xml
 
 # Followed by granting access from a) anywhere or b) from a specific IP address (as in the previous step).
+
+# Install mariadb databases
+sudo apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
+sudo add-apt-repository 'deb [arch=amd64,arm64,ppc64el] https://mariadb.mirror.liquidtelecom.com/repo/10.8/ubuntu focal main'
+
+sudo apt update 
+
+sudo apt install mariadb-server mariadb-client libmariadb-dev -y 
+
+# Remove mariadb strict mode by setting sql_mode = NO_ENGINE_SUBSTITUTION
+sudo rm /etc/mysql/mariadb.conf.d/50-server.cnf
+cd /etc/mysql/mariadb.conf.d/
+wget https://raw.githubusercontent.com/hrmuwanika/vicidial-install-scripts/main/50-server.cnf
+
+sudo systemctl restart mariadb.service
+sudo systemctl enable mariadb.service 
+
+mysql -u root -p << MYSQL_SCRIPT
+create database `fineract_tenants`;
+create database `fineract_default`;
+./gradlew migrateTenantListDB -PdbName=mifosplatform-tenants
+./gradlew migrateTenantDB -PdbName=mifostenant-default
+exit 
+MYSQL_SCRIPT
